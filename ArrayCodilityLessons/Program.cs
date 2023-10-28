17455f1b3960a4f627d09b26d75f269eb45cd813
rb <rkrb@tyfonmail.se>
2023-10-28 13:58:02 +0200

    Rotate an array to the right by a given number of steps

---
+++ ArrayCodilityLessons/Program.cs
@@ -0,0 +1,17 @@
+﻿CyclicRotation test = new CyclicRotation();
+int[] arr = { 3, 8, 9, 7, 6 };
+var test2 = test.Solution(arr, 3);
+foreach (var number in test2)
+{
+    Console.Write(number);
+}
+class CyclicRotation { 
+    public int[] Solution(int[] A, int K)
+    { 
+        Array.Reverse(A);
+        Array.Reverse(A, 0, K);
+        Array.Reverse(A, K, A.Length - K);
+
+        return A;
+    }
+}
